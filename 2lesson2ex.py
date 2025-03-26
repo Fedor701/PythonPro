ef1a0c72d037283f26b1eea4fa6b6d01d52f2a12
Fedor701 <igorrudcenko53@gmail.com>
2025-03-26 22:49:52 +0400

    Delete 2lesson2ex.py

--- 2lesson2ex.py
+++
@@ -1,7 +0,0 @@
-import random
-a = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnmЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю,./?!@#$%^&*()_1234567890"№;%:?='
-b = int(input("Введите количество желаемых символов, для вашего пароля:"))
-s = ''
-for i in range(b):
-    s += random.choice(a)
-print(s)
